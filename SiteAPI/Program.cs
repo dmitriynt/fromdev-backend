5d5f1b474a30f24ce6eb7e1c74d29a513e64f34d
Dmitriy Kiverin <dmitriynt@gmail.com>
2025-01-08 14:19:08 +0500

    remove https, addedd auth in programmcs

--- SiteAPI/Program.cs
+++ SiteAPI/Program.cs
@@ -1,15 +1,20 @@
+using Microsoft.AspNetCore.HttpOverrides;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddAuthentication();
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+});
 
+app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
